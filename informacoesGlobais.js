ced22c4963d48f4f62a2be59edaa2946c1ccf221
Guilherme bauermann <114153918+guilhermepikademel@users.noreply.github.com>
2024-11-05 12:10:59 -0300

    ok

---
+++ informacoesGlobais.js
@@ -0,0 +1,8 @@
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados await res.json()
+    const paragrafo document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+  }
+  
+  vizualizarInformacoesGlobais()
